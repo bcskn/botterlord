01db95f75b0438f51def67b2aaf2d740b3c89d44
Marnsghol <mentalnerd1@gmail.com>
2016-11-11 00:34:55 +0200

    Unknown error when path function is moved to tools file.

--- tools.py
+++ tools.py
@@ -1,3 +1,15 @@
+import os
+
+def get_path(filename = 'main.pt'):
+    """Get self file path."""
+    _path = os.path.realpath('main.py')
+    _lenpath = len(_path)
+    _lenpath -= 7
+    _path = _path[:_lenpath]
+    _path = _path + filename
+    return _path
+
 def parse_str_loc(strloc):
     strloc = strloc.split(':')
     return strloc[0], strloc[1]
+print get_path('main.py')
